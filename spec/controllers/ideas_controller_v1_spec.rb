83d713e0a3ebeb5364237b1d53c4cddeaa454a4d
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-16 17:33:29 -0300

    12° commit - tdd controller: Consegue listar um idea especifico e retornar status 200? RED

--- spec/controllers/ideas_controller_v1_spec.rb
+++ spec/controllers/ideas_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::IdeasController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/ideas/id' do
+    it 'Consegue listar um idea especifico e retornar status 200?' do
+      get :show, params: {id: @idea.id}
+      expect(response.body).to include_json(id: @idea.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
